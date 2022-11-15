4625471840637654c36ef4616fa9b68157118675
dikshantmali <dikshant.mali@unthinkable.co>
2022-11-15 13:05:42 +0530

    add

---
+++ EKS.tf
@@ -0,0 +1,63 @@
+# IAM role for EKS Cluster Master
+
+resource "aws_iam_role" "eks-role" {
+  name = "Personal-prod-eks-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# attach EKS Cluster Policy to the role created above for EKS Master
+resource "aws_iam_role_policy_attachment" "Personal-prod-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-role.name
+}
+
+
+# IAM Role for Nodegroup
+
+resource "aws_iam_role" "node-role" {
+  name = "Personal-prod-eks-nodegrp-role"
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+# attach EKS Cluster Policy to the role created above for EKS Master
+resource "aws_iam_role_policy_attachment" "Personal-prod-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "Personal-prod-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-role.name
+}
+resource "aws_iam_role_policy_attachment" "Personal-prod-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-role.name
+}
